f6bb9124f54898005dc37a91881d34b57744eb7d
Victor Cecilio <60553316+victor12020@users.noreply.github.com>
2023-05-02 13:40:12 -0300

    Add files via upload

--- POO/exercicioDiagrama.js
+++ POO/exercicioDiagrama.js
@@ -6,11 +6,32 @@ class Pessoa{
     }
 
     mostraIdade(){
-        console.log(2023-this.anoNascimento)
+        console.log("A idade é "+(2023-this.anoNascimento))
     }
 }
+
+class Cliente extends Pessoa {
+    constructor(cpf, id){
+    super(cpf);
+    this.id = id
+    }
+    mostraNome(){
+    console.log("O nome é "+this.nome)
+    }
+   }
+
 //programa principal
 let cliente1 = new Pessoa("111.222.333-44","Marcos",1974)
 console.log(cliente1)
-cliente1.mostraIdade()
 console.log(cliente1.nome)
+cliente1.mostraIdade()
+
+let cliente2 = new Cliente("111.222.33-55",1)
+cliente2.anoNascimento = 2000
+cliente2.nome = "Epaminondas"
+//console.log(cliente2)
+cliente2.mostraNome()
+console.log(cliente2.cpf)
+cliente2.mostraIdade()
+
+
