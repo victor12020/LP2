0768c6423b5ccdebce36f1539a4030ba3ddf5225
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-22 11:34:43 -0300

    Finalizando AC

--- Banco/bank.js
+++ Banco/bank.js
@@ -41,10 +41,9 @@ class Conta{
 }
 
 class Poupanca extends Conta{
-    constructor(saldo,aniversario,ativo){
+    constructor(cpf,aniversario,numero,saldo,ativo){
+        super(cpf,numero,saldo,ativo);
         this.aniversario=aniversario;
-        this.saldo=saldo;
-        this.ativo=ativo;
     }
     
     correcao(data){
@@ -97,38 +96,35 @@ console.log("Bem vindo ao banco SALTBANK");
 console.log("Dê um salto na sua vida!")
 let numero = parseInt(leia("Digite o numero da conta : "));
 let cpf = leia("Digite o cpf da conta : ");
-let aniversario=parseInt(leia("Gostariamos de saber a sua data de aniversario : "));
-let C1= new Conta(numero,cpf,0,false);
 let op="";
 let valor=0;
-C1.ativar()
 console.log("SALTBANK");
 console.log("Dê um salto na sua vida!")
 let conta= parseInt(leia("Escolha qual conta deseja usar:\n 1- Poupança \n 2- Corrente \n 3- Especial \n"));
 if(conta==1){
-    let C1p= new Poupanca(0,aniversario,false);
-    C1p.ativar();
-    for(let x=1; x<=10; x++){
+    let aniversario=parseInt(leia("Gostariamos de saber a sua data de aniversario : "));
+    let C1= new Poupanca(cpf,aniversario,numero,0,false);
+    C1.ativar();
+    for(let x=1; x<=1; x++){
         op = leia("Digite D - debito ou C - credito : ");
         console.log("Saldo atual : "+C1.saldo)
         if(op=="D"){
             valor = parseInt(leia("Digite o valor para debito : "))
-            C1p.debitar(valor)
+            C1.debitar(valor)
         }else if(op=="C"){
             valor = parseInt(leia("Digite o valor para creditar : "))
-            C1p.creditar(valor)
+            C1.creditar(valor)
         }
         console.log("Saldo atual da conta : "+C1.saldo)
-        let correcao= leia("Gostaria de fazer a correção do seu saldo? sim/não")
-        if(correcao=="sim"){
-            C1p.correcao(aniversario)
-        } else if(correcao=="não"){
-            console.log("Que pena, deixa pra proxima então.")
-        } else {
-            console.log("desculpa não entendi o que quis dizer.")
-        }
     }
-    
+    let correcao= leia("Gostaria de fazer a correção do seu saldo? sim/não \n")
+    if(correcao=="sim"){
+        let data=leia("Qual a data de hoje? ")
+        C1.correcao(data)
+    } else if(correcao=="não"){
+        console.log("Que pena, deixa pra proxima então.")
+    } else {
+        console.log("desculpa não entendi o que quis dizer.")
+    }
     console.log("Saldo final da conta "+C1.saldo)
 }
-
