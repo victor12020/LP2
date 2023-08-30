855d408e5c7c276cad2a1f617990c4d3f522a310
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-30 08:40:13 -0300

    loginSiteAT

---
+++ node/loginAT/script.js
@@ -0,0 +1,28 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBR5AyuRTqnvd8zu9qTu0QAEryaF_aksFE",
+    authDomain: "projeto2mibteste-cb978.firebaseapp.com",
+    projectId: "projeto2mibteste-cb978",
+    storageBucket: "projeto2mibteste-cb978.appspot.com",
+    messagingSenderId: "527595210917",
+    appId: "1:527595210917:web:6a899e6efb7fa11ff3e54c"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const emailField = document.getElementById('email');
+const passwordField = document.getElementById('password');
+const loginButton = document.getElementById('loginButton');
+
+loginButton.addEventListener('click', () => {
+    const email = emailField.value;
+    const password = passwordField.value;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+    const user = userCredential.user;
+    console.log('Usuário logado:', user);
+    })
+    .catch((error) => {
+    const errorMessage = error.message;
+    console.error('Erro de autenticação:', errorMessage);
+    });
+});
